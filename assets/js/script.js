a5d111986764923afaa88b9fdd7c3235d961e988
Mona Lisa <KEN.MCKELLER92@GMAIL.COM>
2023-07-15 22:24:36 -0500

    updated js and html

---
+++ assets/js/script.js
@@ -0,0 +1,86 @@
+// waits until the doc is loaded and ready to start the functions and variable assignments
+$(document).ready(function () {
+    var myKey = 'eba282579b6e3e0a4f5619443760660f'
+    var localStorageCurrent = localStorage.getItem('pastCities');
+    var localCities = [];
+    var localParsed = JSON.parse(localStorage.getItem('pastCities'));
+    var currentDay = document.querySelector(".current");
+    //this simply appends li's to the ul dynamically based on how many there are saved so I dont have to repeat the code
+    function showHistory() {
+        if (localStorageCurrent) {
+            localParsed.forEach(function (i) {
+                var p = document.createElement('p');
+                p.innerText = i;
+                $('#history').append(p);
+            });
+        };
+    };
+    showHistory();
+    // this function gets the data you searched and finds it on a geo locater api
+    function fetchData(choice) {
+        var geoURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + choice + "&limit=1&appid=" + myKey;
+        fetch(geoURL, {
+            cache: 'reload',
+        }).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            // then puts the found geolocation into the weather apis so they get results for that place
+            var owmURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&appid=' + myKey;
+            var todayWeather = 'https://api.openweathermap.org/data/2.5/weather?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&appid=' + myKey;
+            fetch(owmURL).then(function (response) {
+                return response.json();
+            }).then(function (data) {
+
+                // sets the text of all the attributes below to their corresponding date and time set by the algorithm derived by me
+                
+                $('.5dayFork').each(function (i) {
+                    $(this).find('.date').text(data.list[i * 8 + 4].dt_txt.split(' ')[0]);
+                    $(this).find('.condition').attr('src', '#');
+                    $(this).find('.temp').text('Temperature: ' + data.list[i * 8 + 4].main.temp + 'F');
+                    $(this).find('.wind').text('Wind speed: ' + data.list[i * 8 + 4].wind.speed + 'mph');
+                    $(this).find('.humid').text('Humidity: ' + data.list[i * 8 + 4].main.humidity + '%');
+                });
+            });
+            fetch(todayWeather).then(function (response) {
+                return response.json();
+                // finally sets the same attributes to the today section
+            }).then(function (data) {
+                console.log(data)
+                var weatherIcon = data.weather.icon
+                var img = createElement('img');
+                img.setAttribute("src","https://openweathermap.org/img/wn/" + weatherIcon);
+                $(currentDay).each(function (i) {
+                    $(this).find('.date').text(data.name + ' (' + dayjs().format('MM/DD/YYYY') + ')');
+                    $(this).find('.condition').attr('src', '#');
+                    $(this).find('.temp').text('Temperature: ' + data.main.temp + ', Feels like: ' + data.main.feels_like);
+                    $(this).find('.wind').text('Wind speed: ' + data.wind.speed);
+                    $(this).find('.humid').text('Humidity: ' + + data.main.humidity);
+                });
+            });
+        });
+    };
+    //when the button is clicked, we want to save the input, erase the textbox, and store it in localstorage if it follows our rules.
+    $('button').on('click', function (event) {
+        event.preventDefault();
+        var input = $('#username').val().replaceAll(" ", "")
+        $('#username').val('');
+        if (input) {
+            if (localStorageCurrent && !localParsed.includes(input)) {
+                localParsed.push(input)
+                localStorage.setItem('pastCities', JSON.stringify(localParsed))
+            } else if (!localCities.includes(input)) {
+                localCities.push(input)
+                localStorage.setItem('pastCities', JSON.stringify(localCities))
+            };
+        };
+        //then we show the history and display the data for what you have chosen
+        
+        $('#history').val('');
+        fetchData(input);
+    });
+    // when you click an option on the history list it will run the function that displays the weather with your value
+    $('li').on('click', function () {
+        var choice = $(this).text()
+        fetchData(choice)
+    })
+});
